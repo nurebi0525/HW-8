a4c35fdb6e5098b50f7e5bdac88a7b9ea6a18496
Nurebi Uraimov <nurebi2007@gmail.com>
2025-02-10 07:39:18 +0600

    styled

--- src/components/expenses/expenseFilter.jsx
+++ src/components/expenses/expenseFilter.jsx
@@ -2,24 +2,43 @@ import styled from "styled-components"
 
 export const ExpenseFilter = ({value, onchange}) => {
     return (
-        <div>
+        <Box>
+            <StyledText>Выбран год 2025</StyledText>
             <div>
                 <label htmlFor="filter">Filter By Year</label>
-                <select  id="ilter">
+                <StyledSelect  id="ilter">
                     <option value="2025">2025</option>
                     <option value="2024">2024</option>
                     <option value="2023">2023</option>
                     <option value="2022">2022</option>
                     <option value="2021">2021</option>
-                </select>
+                </StyledSelect>
             </div>
-        </div>
+        </Box>
     );
 };
+
+const Box = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px;
+`;
+
+const STyledLabel = styled.label`
+    margin-right: 20px;
+`
 
 const StyledSelect = styled.select`
     padding: 8px 14px;
     outline: none;
     border-radius: 8px;
     cursor: pointer
+`;
+const StyledText = styled.div`
+   color: #200a2a;
+   background-color: white;
+   padding: 8px 20px;
+   border-radius: 8px;
+   
 `
